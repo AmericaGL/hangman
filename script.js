4dada26ff81ae11529b523ca6fbdb7bd9b3333ec
AmericaGL <info@cybercodetwins.com>
2015-11-19 17:47:20 -0800

    shows the underscores and a letter fills in when the user guesses right

--- script.js
+++ script.js
@@ -29,6 +29,9 @@ var Game = function(word) {
 		for (var i=0; i< word.length; i++){
 		//guessedWord[i] = "_"
 		this.guessedWord[i]="_";
+		console.log(this.guessedWord.join(''));
+		var noSign = this.guessedWord.join(' ');
+		$('#wordstatus').text(noSign);
 		}
 	this.guesses = [];
 	this.isHang = false;
@@ -49,17 +52,24 @@ Game.prototype.guess = function(letter){
 	if(this.correctGuess === this.word.length){
 		this.isHang = true;
 		//code here to change the dom
+
+		alert("You win!");
 	}
 	//this will create a memory box of letters that have been used
 	this.numGuess++;
 	this.guesses.push(letter);
-	console.log(this.guessedWord);
+	 console.log(this.guessedWord.join(''));
+	 var noSign = this.guessedWord.join(' ');
+	 $('#wordstatus').text(noSign);
 };
 
 var playGame = new Game("love");
 
 var guessButton = $('#guessButton').click(function(){
 	var grabValue = $('#guessBox').val();
+	// use joing to 
+	// $('#wordstatus').text(grabValue);
+	//Below keeps record of letters that have been used?
 	playGame.guess(grabValue);
 	$('#guessLetters').append($('<span>'+ grabValue + ' ' + '</span>'));
 });
